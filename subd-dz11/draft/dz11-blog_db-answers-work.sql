4407c324aca6e29ce352582bb0d4f794355ac9d6
Alexander Ponomarenko <aponomarenko@mtt.ru>
2018-12-14 14:58:41 +0300

    dz11 - tmp
    	dz11	Corrected answer #2 at sakila DB
    
    	dz11	draft
    
    	dz11 	tmp
    
    	dz11	update
    
    	dz11 	Fixes
    
    	dz11	blog_db update
    
    	dz11	update
    
    	dz11	corrections

--- subd-dz11/draft/dz11-blog_db-answers-work.sql
+++ subd-dz11/draft/dz11-blog_db-answers-work.sql
@@ -2,10 +2,34 @@
 #адаптированная формулировка задачи к предметной области базы данных: 
 #1. blog_db - Топ 3 статьи с наибольшим количеством лайков по каждому пользователю
 
-#замечание:
 #У вас для пользователя user2 отображается 4 статьи, а не три.
+select * from(
+	select 
+		*, 
+		rank() over (partition by user_id order by like_count desc) as rnk
+	from (
+		select DISTINCT 
+			u.id as user_id,
+			u.name as user_name,
+			p.id as post_id,
+			p.title as post_title,
+			(
+				select count(1) 
+				from blog_db.`like` l2
+				where l2.post_id = l.post_id
+			) as like_count
+		from blog_db.`user` u
+		left outer join blog_db.post p
+			on p.author_user_id = u.id
+		left outer join blog_db.`like` l
+			on l.post_id = p.id
+	) as result
+	order by user_id, like_count desc
+) as result2
+where rnk <= 3
+;
 
-#исправленный вариант с другой оконной функцией, чтобы избежать отображение >3 статей для одного пользователя
+#вариант с другой оконной функцией, чтобы избежать отображение >3 статей для одного пользователя
 select * from(
 	select 
 		*, 
@@ -37,15 +61,33 @@ where rnk <= 3
 #адаптированная формулировка задачи к предметной области базы данных: 
 #2. blog_db - насколько количество лайков по каждой статье отличается от среднего количества лайков по всем статьям
 
-#замечания:
-#1) насколько количество лайков по каждой статье отличается от среднего количества лайков по всем статьям
-   #Я бы хотел видеть в результате просто цифру на сколько количество лайков больше или меньше от среднего значения. 
-   #Среднее у вас примерно 1.19. То есть, если в процентах: 5 лайков - 5*100/1.19 = 420%, 1 лайк - 1*100/1.19 = 84%, 
-   #2 лайка - 2*100/1.19 = 168%
-#2) а почему left outer join ? почему не inner join? p.author_user_id и l.post_id - not null
-#3) И необходимость join blog_db.`like` не понятна.
+#первая версия запроса
+select 
+	*, 
+	percent_rank() over (order by like_count desc) as percent_rank1,
+	CUME_DIST() over (order by like_count desc) as cume_dist1
+from (
+	select DISTINCT 
+		u.id as user_id,
+		u.name as user_name,
+		p.id as post_id,
+		p.title as post_title,
+		(
+			select count(1) 
+			from blog_db.`like` l2
+			where l2.post_id = l.post_id
+		) as like_count
+	from blog_db.`user` u
+	left outer join blog_db.post p
+		on p.author_user_id = u.id
+	left outer join blog_db.`like` l
+		on l.post_id = p.id
+) as results	
+order by post_id	
+;
 
-#исправленное решение: 
+
+#вторая версия запроса
 #1) убрал join blog_db.`like`, т.к. это лишняя операция 
 #2) посчитал в 
 #		sum1 - общее количество лайков, 
@@ -65,14 +107,50 @@ from blog_db.post p
 )
 select 
 	*, 
-	(select sum(like_count) from posts_like_count) as total_likes_count,
-	(select count(1) from posts_like_count) as posts_count,
-	(select total_likes_count / posts_count) as middle1,
+	(select sum(like_count) from posts_like_count) as sum1,
+	(select count(1) from posts_like_count) as cnt1,
+	(select sum1 / cnt1) as middle1,
 	(select concat(format(like_count / middle1 * 100, 'N'), '%')) as percent_to_middle1
 from posts_like_count
 ;
 
-#заменил count на оконные функции, но пришлось добавить JOIN'ы
+
+#третья версия запроса - уйти от count()
+#внутренний запрос
+select 
+	posts_like_count_lineno.id,
+	posts_like_count_lineno.title,
+	posts_like_count_lineno.like_count,
+	LAST_VALUE(line_no) over (PARTITION BY posts_like_count_lineno.anchor) as posts_count
+from (
+	with posts_like_count_tmp as(
+		select DISTINCT
+			post_id,
+			LAST_VALUE(rank1) over (PARTITION BY post_id) as like_count
+		from (
+			select  
+				p.id as post_id,
+				ROW_NUMBER() over (PARTITION BY p.id) as rank1
+			from blog_db.post p
+			inner join blog_db.`like` as l
+				on l.post_id = p.id
+		) as sss
+	)
+	select 
+		p.id,
+		p.title,
+		(SELECT IF (plc.like_count IS NOT NULL, plc.like_count, '0')) as like_count,
+		ROW_NUMBER() over (order by p.id) as line_no,
+		(select 1) as anchor
+	from blog_db.post p	
+	left outer join posts_like_count_tmp plc
+		on plc.post_id = p.id
+) as posts_like_count_lineno
+;
+
+
+
+#третья версия запроса - уйти от count() - ВСЕ В СБОРЕ
 with posts_like_count as(
 	select 
 		posts_like_count_lineno.id,
@@ -115,15 +193,12 @@ from posts_like_count
 
 
 
+
+
 #3. Выберите 5 лучших продавцов (работников, водителей) по сумме проданного.
 #адаптированная формулировка задачи к предметной области базы данных: 
 #3. blog_db - 3 наиболее активных автора (написали наибольшее число статей)
-
-#замечания:
-#1) В вашем решении достаточно в подзапросе написать order by и limit 3, внешний запрос и не нужен
-#2) Попробуйте переписать этот запрос без подзапросов. Это возможно как раз с использованием оконных функций.
-
-#решение - шаг 1. Использовал order by и limit 3 
+#3. версия запроса №2 
 select 
 	u.id, 
 	u.name,
@@ -132,7 +207,7 @@ from blog_db.`user` u
 limit 3
 ;
 
-#решение - шаг 2. заменяю count(1) на оконные функции
+#3. версия запроса №3 
 with user_id_name_rank as (
 	select 
 		u.id, 
@@ -152,14 +227,32 @@ limit 3
 
 
 
+
+#3. версия запроса №1
+select * from(
+	select 
+		*,
+		dense_rank() over (order by post_count desc) as rnk
+	from(
+		select 
+			u.id, 
+			u.name,
+			(select count(1) from blog_db.post p where p.author_user_id = u.id) as post_count
+		from blog_db.`user` u
+	) as results1
+) as results2
+where rnk <= 3
+;
+
+
+
+
+
+
 #4. Топ 2 самые новых новостей по темам
 #адаптированная формулировка задачи к предметной области базы данных: 
 #4. blog_db - Вывести 2 наиболее интересных статьи (просмотрены наибольшее количество раз)
-
-#замечание:
-#Аналогично предыдущему запросу попробуйте переписать без подзапросов.
-
-#решение:
+#4. новый запрос
 select 
 	uvp.post_id, 
 	p.title,
@@ -171,6 +264,28 @@ group by uvp.post_id
 order by total_views_count desc
 limit 2
 ;
+
+
+#4. старый запрос
+select * from(
+	select 
+		*,
+		dense_rank() over(order by total_views_count desc) as rnk
+	from (
+		select 
+			uvp.post_id, 
+			p.title,
+			sum(uvp.views_no) as total_views_count
+		from blog_db.user_viewed_post uvp
+		left outer join blog_db.post p
+			on p.id = uvp.post_id
+		group by uvp.post_id
+	) as results1
+	order by total_views_count desc
+) as results2
+where rnk <= 2 
+;
+
 
 
 
--- subd-dz11/draft/selects.sql
+++ subd-dz11/draft/selects.sql
@@ -458,3 +458,469 @@ order by actor_id1
 ;
 
 
+select distinct r4.rental_date 
+from rental r4 
+where r4.staff_id=s.staff_id
+order by rental_date desc
+limit 1
+;
+
+select 
+	dense_rank() over (order by r1.rental_date desc) as rn,
+	first_value(rental_id) over (partition by r1.rental_date) as first_val,
+	lag(rental_id) over (partition by r1.rental_date) as lag1,
+	r1.*  
+from sakila.rental r1
+order by rn
+;
+
+
+select count(1)
+from sakila.rental
+;
+
+with rental2 as (
+	select 
+		rental1.rental_id, 
+		rental1.staff_id, 
+		rental1.rental_date, 
+		rental1.inventory_id,
+		rental1.customer_id
+	from (
+		select
+			r.rental_id,
+			r.staff_id,
+			r.rental_date, 
+			r.inventory_id,
+			r.customer_id,
+			ROW_NUMBER() over (partition by r.staff_id, r.rental_date order by r.staff_id, r.rental_date desc) as rn
+		from sakila.rental r
+		order by r.rental_date desc, rn, r.staff_id
+	) as rental1
+	where rn <= 1
+	order by rental1.rental_date desc
+)
+select * from rental2;
+
+#по каждому фильму - дата последнего просмотра
+with film_last_view_date as (
+	#по каждому инвентарному номеру - дату последнего просмотра
+	with rental2 as (
+		select 
+			rental1.rental_id, 
+			rental1.staff_id, 
+			rental1.rental_date, 
+			rental1.inventory_id,
+			rental1.customer_id
+		from (
+			select
+				r.rental_id,
+				r.staff_id,
+				r.rental_date, 
+				r.inventory_id,
+				r.customer_id,
+				ROW_NUMBER() over (partition by r.staff_id, r.rental_date order by r.staff_id, r.rental_date desc) as rn
+			from sakila.rental r
+			order by r.rental_date desc, rn, r.staff_id
+		) as rental1
+		where rn <= 1
+		order by rental1.rental_date desc
+	)
+	select 
+		r.rental_id,
+		r.staff_id,
+		r.rental_date,
+		r.inventory_id,
+		r.customer_id,
+		i.film_id,
+		f.title as film_title
+	from rental2 r
+	inner join sakila.inventory i
+		on i.inventory_id = r.inventory_id
+	inner join sakila.film f
+		on f.film_id = i.film_id
+)
+select * 
+from film_last_view_date
+;
+
+
+
+
+
+
+
+#неправильно
+with rental2 as (
+	select 
+		rental1.rental_id, 
+		rental1.staff_id, 
+		rental1.rental_date, 
+		rental1.inventory_id,
+		rental1.customer_id
+	from (
+		select
+			r.rental_id,
+			r.staff_id,
+			r.rental_date, 
+			r.inventory_id,
+			r.customer_id,
+			ROW_NUMBER() over (partition by r.staff_id, r.rental_date order by r.staff_id, r.rental_date desc) as rn
+		from sakila.rental r
+		order by r.rental_date desc, rn, r.staff_id
+	) as rental1
+	where rn <= 1
+	order by rental1.rental_date desc
+)
+select 
+	actor_id1,
+	actor_first_name,  
+	actor_last_name,  
+	film_id,  
+	film_title,  
+	last_rental_date,
+	rank1
+from (
+	select 
+		a.actor_id as actor_id1, 
+		a.first_name as actor_first_name, 
+		a.last_name as actor_last_name, 
+		fa.film_id, 
+		f.title as film_title,
+		i.inventory_id,
+		r.rental_id,
+		r.rental_date as last_rental_date,
+		rank() over (partition by a.actor_id order by r.rental_date desc) as rank1
+	from sakila.actor a
+	left outer join sakila.film_actor fa
+		on fa.actor_id = a.actor_id
+	left outer join sakila.film f
+		on f.film_id = fa.film_id
+	left outer join sakila.inventory i
+		on i.film_id = fa.film_id
+	left outer join rental2 r
+		on r.inventory_id = i.inventory_id
+) as result1
+where rank1 <= 10
+;
+
+order by actor_id1
+
+select *
+from sakila.rental r
+where 
+;
+
+
+select distinct
+	r.staff_id,
+	r.rental_date 
+from sakila.rental r
+inner join sakila.rental r2
+	on r.rental_id = r2.rental_id
+order by r.rental_date desc, r.staff_id
+;
+
+
+#4 - вариант с аналитической функцией. Вывести для каждого актера только один фильм, посмотренный последним
+select  
+	actor_id1, 
+	actor_first_name,
+	actor_last_name,
+	film_id,
+	film_title,
+	rental_date
+from (
+	with actor_last_views as (
+	select * from (
+		select 
+			a.actor_id as actor_id1, 
+			a.first_name as actor_first_name, 
+			a.last_name as actor_last_name, 
+			fa.film_id, 
+			f.title as film_title,
+			i.inventory_id,
+			r.rental_id,
+			r.rental_date as rental_date,
+			rank() over (partition by a.actor_id order by r.rental_date desc) as rank1
+		from sakila.actor a
+		left outer join sakila.film_actor fa
+			on fa.actor_id = a.actor_id
+		left outer join sakila.film f
+			on f.film_id = fa.film_id
+		left outer join sakila.inventory i
+			on i.film_id = fa.film_id
+		left outer join sakila.rental r
+			on r.inventory_id = i.inventory_id
+	) as result1	
+	where rank1 <= 1
+	order by actor_id1
+	)
+	select 
+		actor_id1, 
+		actor_first_name,
+		actor_last_name,
+		film_id,
+		film_title,
+		rental_date,
+		ROW_NUMBER() over (partition by actor_first_name, actor_last_name order by actor_first_name, actor_last_name) as rank2
+	from actor_last_views
+) as actor_last_views_with_rank
+where rank2 <= 1
+;
+
+
+
+
+
+#############
+#2. Насколько зарплата конкретного человека отличается от средней зарплаты (или насколько чек отличается от среднего чека) 
+#адаптированная формулировка задачи к предметной области базы данных: 
+#2. blog_db - насколько количество лайков по каждой статье отличается от среднего количества лайков по всем статьям
+with posts_like_count as (
+select DISTINCT 
+	p.id as post_id,
+	p.title as post_title,
+	(
+		select count(1) 
+		from blog_db.`like` l2
+		where l2.post_id = p.id
+	) as like_count
+from blog_db.post p
+)
+select 
+	*, 
+	(select sum(like_count) from posts_like_count) as sum1,
+	(select count(1) from posts_like_count) as cnt1,
+	(select sum1 / cnt1) as middle1,
+	(select concat(format(like_count / middle1 * 100, 'N'), '%')) as percent_to_middle1
+from posts_like_count
+;
+
+#2. старый запрос
+select 
+	*, 
+	percent_rank() over (order by like_count desc) as percent_rank1,
+	CUME_DIST() over (order by like_count desc) as cume_dist1
+from (
+	select DISTINCT 
+		u.id as user_id,
+		u.name as user_name,
+		p.id as post_id,
+		p.title as post_title,
+		(
+			select count(1) 
+			from blog_db.`like` l2
+			where l2.post_id = l.post_id
+		) as like_count
+	from blog_db.`user` u
+	left outer join blog_db.post p
+		on p.author_user_id = u.id
+	left outer join blog_db.`like` l
+		on l.post_id = p.id
+) as results	
+order by post_id	
+;
+
+
+#3. Выберите 5 лучших продавцов (работников, водителей) по сумме проданного.
+#адаптированная формулировка задачи к предметной области базы данных: 
+#3. blog_db - 3 наиболее активных автора (написали наибольшее число статей)
+select 
+	*,
+	dense_rank() over (order by post_count desc) as rnk
+from(
+	select 
+		u.id, 
+		u.name,
+		(select count(1) from blog_db.post p where p.author_user_id = u.id) as post_count
+	from blog_db.`user` u
+) as results1
+limit 3
+;
+
+
+#старый запрос:
+select * from(
+	select 
+		*, 
+		rank() over (partition by user_id order by like_count desc) as rnk
+	from (
+		select DISTINCT 
+			u.id as user_id,
+			u.name as user_name,
+			p.id as post_id,
+			p.title as post_title,
+			(
+				select count(1) 
+				from `like` l2
+				where l2.post_id = l.post_id
+			) as like_count
+		from `user` u
+		left outer join post p
+			on p.author_user_id = u.id
+		left outer join `like` l
+			on l.post_id = p.id
+	) as result
+	order by user_id, like_count desc
+) as result2
+where rnk <= 3
+;
+
+
+
+
+
+
+select * from(
+	select 
+		*,
+		dense_rank() over (order by post_count desc) as rnk
+	from(
+		select 
+			u.id, 
+			u.name,
+			(select count(1) from blog_db.post p where p.author_user_id = u.id) as post_count
+		from blog_db.`user` u
+	) as results1
+) as results2
+where rnk <= 3
+;
+
+
+#4. Топ 2 самые новых новостей по темам
+#адаптированная формулировка задачи к предметной области базы данных: 
+#4. blog_db - Вывести 2 наиболее интересных статьи (просмотрены наибольшее количество раз)
+#4. новый запрос
+select 
+	uvp.post_id, 
+	p.title,
+	sum(uvp.views_no) as total_views_count
+from blog_db.user_viewed_post uvp
+left join blog_db.post p
+	on p.id = uvp.post_id
+group by uvp.post_id
+order by total_views_count desc
+limit 2
+;
+
+
+#4. старый запрос
+select * from(
+	select 
+		*,
+		dense_rank() over(order by total_views_count desc) as rnk
+	from (
+		select 
+			uvp.post_id, 
+			p.title,
+			sum(uvp.views_no) as total_views_count
+		from blog_db.user_viewed_post uvp
+		left outer join blog_db.post p
+			on p.id = uvp.post_id
+		group by uvp.post_id
+	) as results1
+	order by total_views_count desc
+) as results2
+where rnk <= 2 
+;
+
+#вычисление общего количества записей через оконные функции
+select 
+tbl1.film_id,
+tbl1.title,
+LAST_VALUE(rn) over (partition by tbl1.col1) as count
+from (
+	select 
+		f.film_id, 
+		f.title,
+		ROW_NUMBER() over (order by f.film_id) as rn,
+		(select 1) as col1
+	from sakila.film f
+) as tbl1
+;
+
+#прямой запрос count для вычисления общего количества записей
+select 
+	f.film_id, 
+	f.title,
+	(select count(1) from sakila.film) as count
+from sakila.film f
+window w as (order by f.film_id)
+;
+
+
+
+LAST_VALUE(rn) over (partition by tbl1.col1) as lv2
+
+
+		(select count(1) from sakila.film) as count,
+window w2 as (partition by tbl1.col1 order by tbl1.film_id)
+
+
+
+#сложный запрос с вычисление количества записей в лоб
+select 
+	rank1, 
+	LAST_VALUE(rank1) over w as count_rank,
+	id,
+	next_id,
+	prev_id,
+	prev_name2,
+	title,
+	descr,
+	year,
+	count,
+	LAST_VALUE(rank2) over (partition by results.anchor) as count2
+from (
+	select 
+		ROW_NUMBER() over w as rank1, 
+		sakila.film.film_id as id, 
+		lead(sakila.film.film_id) over w as next_id, 
+		lag(sakila.film.film_id) over w as prev_id, 
+		lag(sakila.film.title, 2) over w as prev_name2, 
+		(title), 
+		(sakila.film.description) as descr, 
+		(sakila.film.release_year) as year,
+		(select count(1) from sakila.film) as count,
+		ROW_NUMBER() over (order by film_id) as rank2,
+		(select 1) as anchor
+	from sakila.film 
+	window w as (partition by left(title, 1) order by left(title, 1))
+	order by sakila.film.film_id
+	) as results
+	window w as (partition by left(title, 1) order by left(title, 1))
+;
+
+
+#сложный запрос с вычислением количества записей при помощи оконных функций
+select 
+	rank1, 
+	LAST_VALUE(rank1) over w as count_rank,
+	id,
+	next_id,
+	prev_id,
+	prev_name2,
+	title,
+	descr,
+	year,
+	LAST_VALUE(rank2) over (partition by results.anchor) as count
+from (
+	select 
+		ROW_NUMBER() over w as rank1, 
+		sakila.film.film_id as id, 
+		lead(sakila.film.film_id) over w as next_id, 
+		lag(sakila.film.film_id) over w as prev_id, 
+		lag(sakila.film.title, 2) over w as prev_name2, 
+		(title), 
+		(sakila.film.description) as descr, 
+		(sakila.film.release_year) as year,
+		ROW_NUMBER() over (order by film_id) as rank2,
+		(select 1) as anchor
+	from sakila.film
+	window w as (partition by left(title, 1) order by left(title, 1))
+	order by sakila.film.film_id
+	) as results
+	window w as (partition by left(title, 1) order by left(title, 1))
+;
+ 
